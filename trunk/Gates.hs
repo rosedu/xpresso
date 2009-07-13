a45a9196bee1f5d991dabd623f66bea27d1f0c0b
Irina Gradinaru <steamspring@gmail.com>
2009-07-13 17:15:25 +0000

    terminat getGates pentru circuite AndOr, numele portilor nu sunt sugestive dar functioneaza

--- trunk/Gates.hs
+++ trunk/Gates.hs
@@ -24,7 +24,7 @@ data Options = Options {
 {- gets the gates to implement an expression according to certain options -}
 getGates :: String -> Options -> [GateLevel]
 getGates str opts = case circType opts of
-		AndOr -> getOrGate vars $ 
+		AndOr -> getOrGate opts vars $ 
 			(\x -> minAndOr  x (pAnd opts) (pNor opts)) 
 			$ getMinImps $ str
     where	 
@@ -33,13 +33,39 @@ getGates str opts = case circType opts of
 
 {- given a list of variable names and a list of implicants, returns the
 	corresponding OR gate plus an AND gate for each implicant -}
-getOrGate :: [String] -> [Implicant] -> [GateLevel]
-getOrGate vars imps = orGate ++ andGates
+getOrGate :: Options -> [String] -> [Implicant] -> [GateLevel]
+getOrGate opts vars imps = orGate ++ (concat andGates)
     where
-    	andGates = concat $ map (getAndGate vars) imps 
-	orGate = if (length imps <= 1) then [] else
-		(GateLevel name "OR" (map glValue andGates)):[]
+    	andGates = map (splitGate (pAnd opts))
+				$ concat $ map (getAndGate vars) imps 
+	lastGates = map last andGates
+	singleImps = map (fst.head.filter (\x -> snd x == Care True)) 
+			$ map (zip vars) $ filter singleImp imps
+	singleImp imp = length (filter (== Care True) imp) == 1 &&
+			length (filter (== Care False) imp) == 0
+	orGate = if (length imps <= 1) then [] else splitGate (pOr opts) 
+	    	(GateLevel name "OR" ((map glValue lastGates) ++ singleImps ))
 	name = tail $ concat $ map (\x -> "+"++show x) imps
+
+splitGate :: Int -> GateLevel -> [GateLevel]
+splitGate p gate = if p >= length (glVars gate) then [gate] else
+	childGates ++ splitGate p motherGate
+    where
+    	tempGates = map makeGate $ zip3 (splitName (glValue gate)) 
+		      		 	(repeat (glType gate)) 
+		      			(splitVars p (glVars gate))
+	childGates = if (length . glVars . last) tempGates == 1
+		then init tempGates else tempGates
+	residualInput = if (length . glVars . last) tempGates /= 1 then [] 
+		else (glVars . last) tempGates
+ 	makeGate = \(x,y,z) -> (GateLevel x y z)
+	motherGate = makeGate (newName, glType gate, 
+				(map glValue childGates) ++ residualInput)
+	newName = (glValue gate) ++ "-" ++ (show . length . glVars) gate
+	splitName name = map (\x -> name++"_"++show x) [1..] 
+	splitVars p list = if length list <= p then [list] else
+		(take p list):(splitVars p (drop p list))
+
 
 {- given a list of variable names and an implicant, returns the corresponding
 	AND gate and NOT gates if needed -}
