932df2841afaff234899e0876bd3d98d7eb6d463
Irina Gradinaru <steamspring@gmail.com>
2009-07-14 21:44:47 +0000

    added getNandGates and getNorGates functions

--- trunk/Gates.hs
+++ trunk/Gates.hs
@@ -9,7 +9,7 @@ data GateLevel = GateLevel {
     glValue ::  String,
     glType  ::  String,
     glVars  ::  [String]
-} deriving Show
+} deriving (Show, Eq)
 
 data CircuitType = AndOr | Nand | Nor
 
@@ -27,7 +27,13 @@ getGates str opts = case circType opts of
 		AndOr -> getOrGate opts vars $ 
 			(\x -> minAndOr  x (pAnd opts) (pNor opts)) 
 			$ getMinImps $ str
-    where	 
+ 		Nand -> getNandGate opts vars $ (\x->minAndOr x inf inf) $ 
+					getMinImps $ str
+		Nor -> getNorGate opts vars $ (\x->minAndOr x inf inf) $ 
+					getMinImps $ str
+
+   where	 
+   	inf  = floor (1/0) 
 	expr = fromJust $ play str
 	vars = keys $ getVars expr
 
@@ -47,6 +53,84 @@ getOrGate opts vars imps = orGate ++ (concat andGates)
 	    	(GateLevel name "OR" ((map glValue lastGates) ++ singleImps ))
 	name = tail $ concat $ map (\x -> "+"++show x) imps
 
+{- given a list of variable names and an implicant, returns the corresponding
+	AND gate and NOT gates if needed -}
+getAndGate :: [String] -> Implicant ->  [GateLevel]
+getAndGate vars imp = andGate ++ notGates 
+    where
+    	notGates = map fromJust $ filter isJust $ map getNotGate varPairs 
+	andGate = if (length (filter (/= Dash) imp) <= 1 ) then [] else
+		GateLevel varName "AND" 
+		(filter (/= []) (map getName varPairs)) : []
+	varName = concat (map show imp)
+	varPairs = zip imp vars
+	getName (token, var) 
+	    | token == Dash = []
+	    | token == Care True = var
+	    | token == Care False = var++"'"
+
+getNorGate :: Options -> [String] -> [Implicant] -> [GateLevel]
+getNorGate opts vars imps = map transform motherGate
+    where
+    	transform gate = if (glType gate /= "NAND") then gate else
+		GateLevel (glValue gate) "NOR" (glVars gate)
+    	motherGate = if notNegated == [] then [] else
+		if (glType.head) notNegated == "NAND" then 
+		GateLevel name "NOT" [(glValue (head notNegated))] : notNegated
+		else notNegated
+	name = "(" ++ (glValue (head notNegated)) ++ ")'"
+    	notNegated = getNandGate opts vars negatedImps
+	negatedImps = map (\x -> if (length (filter (/= Dash) x) == 1) 
+		then x else negateImp x) imps 
+	negateImp [] = [] 
+	negateImp (x:lx) = case x of 
+		Care True -> Care False 
+		Care False-> Care True
+		Dash	  -> Dash
+		: (negateImp lx)
+
+getNandGate :: Options -> [String] -> [Implicant] -> [GateLevel]
+getNandGate opts vars imps = if length childNands == 1 then 
+	    if (length.filter (/=Dash).head) imps == 1 then 
+	    getAndGate vars (head imps)
+	    else
+   	    [GateLevel ("("++name++")'") "NOT" (map glValue lastGates)] 
+	    			++ (concat childNands)
+	    else
+ 	    [GateLevel ("("++name++")'") "NAND" 
+	    	(map glValue lastGates ++ singleImps)] ++ (concat childNands)
+    where
+    	childNands = map (getChildNand vars) imps
+	singleImps = map (fst.head.filter (\x -> snd x == Care False)) 
+			$ map (zip vars) $ filter singleImp imps
+	singleImp imp = length (filter (== Care False) imp) == 1 &&
+			length (filter (== Care True) imp) == 0
+	name = tail $ concat $ map (\x -> "+"++show x) imps
+	lastGates = map last (filter (/=[]) childNands)
+
+getChildNand :: [String] -> Implicant -> [GateLevel]
+getChildNand vars imp = if (length (filter (/=Dash) imp) <= 1 ) then singleNot
+			else notGates ++ nandGate
+    where
+	nandGate = [GateLevel varName "NAND" 
+			(filter ( /= []) (map getName varPairs))]
+   	notGates = map fromJust $ filter isJust $ map getNotGate varPairs 
+	singleNot = if fst singleImp == Care True then 
+		[(fromJust.getNotGate) (Care False, snd singleImp)] else []
+	singleImp = head (filter (\(x,y) -> x /= Dash) varPairs)
+	varName = "(" ++ (concat (map show imp)) ++ ")'"
+	varPairs = zip imp vars
+	getName (token, var) 
+	    | token == Dash = []
+	    | token == Care True = var
+	    | token == Care False = var++"'"
+
+{- if the QuineToken given is negated, it returns a corresponding NOT gate -}
+getNotGate :: (QuineToken, String) -> Maybe GateLevel
+getNotGate (token, var) = if token == Care False then 
+		Just (GateLevel (var++"'") "NOT" [var])
+		else Nothing
+
 splitGate :: Int -> GateLevel -> [GateLevel]
 splitGate p gate = if p >= length (glVars gate) then [gate] else
 	childGates ++ splitGate p motherGate
@@ -65,27 +149,3 @@ splitGate p gate = if p >= length (glVars gate) then [gate] else
 	splitName name = map (\x -> name++"_"++show x) [1..] 
 	splitVars p list = if length list <= p then [list] else
 		(take p list):(splitVars p (drop p list))
-
-
-{- given a list of variable names and an implicant, returns the corresponding
-	AND gate and NOT gates if needed -}
-getAndGate :: [String] -> Implicant ->  [GateLevel]
-getAndGate vars t = andGate ++ notGates 
-    where
-    	notGates = map fromJust $ filter isJust $ 
-	    map getNotGate varPairs 
-	andGate = if (length (filter (/= Dash) t) <= 1 ) then [] else
-		GateLevel varName "AND" 
-		(filter (/= []) (map getName varPairs)) : []
-	varName = concat (map show t)
-	varPairs = zip t vars
-	getName (token, var) 
-	    | token == Dash = []
-	    | token == Care True = var
-	    | token == Care False = var++"'"
-
-{- if the QuineToken given is negated, it returns a corresponding NOT gate -}
-getNotGate :: (QuineToken, String) -> Maybe GateLevel
-getNotGate (token, var) = if token == Care False then 
-		Just (GateLevel (var++"'") "NOT" [var])
-		else Nothing
