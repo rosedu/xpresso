290598eed75fc248a5f1557e388a77f441d4e658
Mihai Maruseac <mihai.maruseac@rosedu.org>
2009-07-17 10:36:13 +0000

    Mapari intre cele 2 structuri de componente, ar trebui sa pot incepe desenarea acum

--- trunk/IDraw.hs
+++ trunk/IDraw.hs
@@ -2,38 +2,68 @@ module IDraw where
 
 import Data.List ((\\), intersect)
 
+{-Structural and combinational component-}
 data Component = Component {
     cType :: String,
     cInputs :: [String],
     cOutputs :: [String]
     } deriving (Eq, Show)
 
+{- Resulting SVG point -}
 data SVGPoint = SVGPoint {
     svgX :: Int,
     svgY :: Int
     } deriving (Eq, Show)
 
+{- Resulting SVG component port -}
 data SVGPort = SVGPort {
     svgStart :: SVGPoint,
     svgStop :: SVGPoint,
     svgLabel :: String
     } deriving (Eq, Show)
 
+{- Resulting SVG component:
+ - svgcDefs === the SVG description of the component
+ --}
 data SVGComponent = SVGComponent {
     svgcType :: String,
     svgcPorts :: [SVGPort],
     svgcDefs :: String
     } deriving (Eq, Show)
 
-type Node = Component
-type Edge = (Component, Component)
+{- Resulting SVG path between two omponents -}
+data SVGPath = SVGPath {
+    svgcControls :: [SVGPoint]
+    } deriving (Eq, Show)
 
+{- Localized instance of a SVGComponent -}
+data SVGPComponent = SVGPComponent {
+    svgpInstance :: SVGComponent,
+    svgCorner :: SVGPoint
+    } deriving (Eq, Show)
+
+{- Mapping between wires and port names -}
+data WireMap = WireMap {
+    wireName :: String,
+    portStart :: String,
+    portEnd :: String
+    } deriving (Eq, Show)
+
+{-tobedeleted-}
 nodes = [Component "AND" ["i", "w"] ["x"], 
       Component "BQ" ["xx", "c"] ["w", "Q'"],
-      Component "ID" ["x"] ["xx"]]
+      Component "ID" ["x"] ["xx"],
+      Component "ID" ["t"] ["c"]]
 edges = [(Component "AND" ["i", "w"] ["x"], Component "ID" ["x"] ["xx"]),
       (Component "ID" ["x"] ["xx"], Component "BQ" ["xx", "c"] ["w", "Q'"]),
-      (Component "BQ" ["xx", "c"] ["w", "Q'"], Component "AND" ["i", "w"] ["x"])]
+      (Component "BQ" ["xx", "c"] ["w", "Q'"], Component "AND" ["i", "w"] ["x"]),
+      (Component "ID" ["t"] ["c"], Component "BQ" ["xx", "c"] ["w", "Q'"])]
+parsedSVGComponents = [
+    SVGComponent "AND" [SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "A", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "B", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "X"] "nodefyet",
+    SVGComponent "ID" [SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "A", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "X"] "nodefyet",
+    SVGComponent "BQ" [SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "D", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "T", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "Q", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "Q'"] "nodefyet",
+    SVGComponent "MUX" [SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "A", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "B", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "C", SVGPort (SVGPoint 2 1) (SVGPoint 2 1) "X"] "nodefyet"]
+{-untilhere-}
 
 striplevel :: [Component] -> [Component]
 striplevel schema  = schema \\ last
@@ -46,6 +76,29 @@ striplevel schema  = schema \\ last
 
 getLevels :: [Component] -> [[Component]]
 getLevels [] = []
-getLevels n = [head n] : (zipWith (\\) l $ tail l)
+getLevels n = [head n] : (reverse . zipWith (\\) l $ tail l)
     where
 	l = takeWhile (/= []) $ iterate striplevel n
+
+getUsedSVGComponents :: [Component] -> [SVGComponent] -> 
+    [(Component, SVGComponent)]
+getUsedSVGComponents [] svg = []
+getUsedSVGComponents (c:cs) svg = l : (getUsedSVGComponents cs svg)
+    where
+	l = (c, head $ filter f svg)
+	f x = svgcType x == cType c
+
+getGateInputWireMapping :: Component -> SVGComponent -> [(String, String)]
+getGateInputWireMapping c svg = zip (cInputs c) (map svgLabel $ svgcPorts svg)
+
+getGateOutputWireMapping :: Component -> SVGComponent -> [(String, String)]
+getGateOutputWireMapping c svg = reverse $ zip (reverse $ cOutputs c) 
+    (reverse $ map svgLabel $ svgcPorts svg)
+
+getWireMapping :: [(Component, SVGComponent)] -> [WireMap]
+getWireMapping pairs = error . show $ output
+    where
+	input = map f pairs
+	f x = getGateInputWireMapping (fst x) (snd x)
+	output = map f' pairs
+	f' x = getGateOutputWireMapping (fst x) (snd x)
